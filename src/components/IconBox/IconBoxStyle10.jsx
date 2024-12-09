cdf13e9c321d89bb5402e20b90569d2927227034
Aman kumar ujjain <amankrujjain@gmail.com>
2024-12-09 14:35:35 +0530

    icon color changed to red

--- src/components/IconBox/IconBoxStyle10.jsx
+++ src/components/IconBox/IconBoxStyle10.jsx
@@ -4,7 +4,7 @@ export default function IconBoxStyle10({ title, iconSrc, subTitle }) {
   return (
     <div className="cs_iconbox cs_style_10 cs_radius_20 cs_white_bg text-center">
       <div className="cs_iconbox_icon cs_center rounded-circle">
-        <img src={iconSrc} alt="Icon" />
+        <img style={{height:'45px'}} src={iconSrc} alt="Icon" />
       </div>
       <h2 className="cs_iconbox_title" style={{fontSize:'24px', color:'#2C2F76'}}>{title}</h2>
       <p className="cs_iconbox_subtitle mb-0">{subTitle}</p>
